68213b9b8652be42680bb187c1a79a19e5154ac4
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-11-19 16:15:57 -0500

    Field Controller & UI Updates

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour {
+
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()  {
+        
+    }
+
+    // Update is called once per frame
+    void Update()  {
+        
+    }
+
+    public void OnInventoryButtonDown() {
+        Debug.Log("Bring up Inventory.");
+        panel.SetActive(true);
+    }
+
+    public void OnInventoryExitButtonDown() {
+        panel.SetActive(false);
+    }
+}
