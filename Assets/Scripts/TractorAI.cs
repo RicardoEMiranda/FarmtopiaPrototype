3fb21e29253b94249884f3fcb1c7061c7f7a2ae1
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-11-23 18:54:44 -0500

    Simulated crop harvesting.
    These updates include the Tractor AI, SFX, and crop colliders that detect when the tractor has collided with crops. Once tractor collides with crops, the crops are destroyed and a harvesting sound effect is played.
    
    Included particle system for tractor.

--- Assets/Scripts/TractorAI.cs
+++ Assets/Scripts/TractorAI.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class TractorAI : MonoBehaviour {
 
-    private float speed = .5f;
-    private int direction = -1;
+    public float speed = .5f;
+    private int direction = 1;
     private float orientation;
 
     // Start is called before the first frame update
     void Start()  {
-        orientation = gameObject.transform.localRotation.y;
+        
+        //orientation = gameObject.transform.localRotation.y;
     }
 
     // Update is called once per frame
     void Update() {
         //Debug.Log(orientation);
-        transform.Translate(new Vector3(0,0,1)*Time.deltaTime*speed*direction);
+        //transform.Translate(new Vector3(0,0,1)*Time.deltaTime*speed*direction);
 
-        if(gameObject.transform.position.z <=-1.5f)  {
-            //Debug.Log("Position Hit");
-            orientation = 180;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+       
+
+        if(gameObject.transform.position.x >=-234.0f && gameObject.transform.position.x <= -207.0f )  {
+            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed * direction);
         }
 
         if(gameObject.transform.position.z >= 5.3)  {
-            orientation = 0;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            //orientation = 0;
+            //transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         }
     }
 }
