f29302a1a33da895dbb1643092bbea225cbc464f
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-11-25 15:28:57 -0500

    Speech Bubble Updates
    Created canvas that is attached to player character.
    Created temporary speech bubble solution and a
    "LookAtPlayer" script so that speech bubble faces the camera.
    
    But bubble is slightly off angle and will need to figure out what's going on there.

--- Assets/Scripts/FarmerAI_Test.cs
+++ Assets/Scripts/FarmerAI_Test.cs
@@ -7,8 +7,12 @@ public class FarmerAI_Test : MonoBehaviour {
     [SerializeField] public Transform[] waypoints;
     [SerializeField] public Vector3 currentPos;
     [SerializeField] public Vector3 endPos;
+    [SerializeField] public float speed = 2f;
     public Vector3 navigationVector;
     public float delta;
+
+    public float theta;
+    private float rotAngle;
 
     // Start is called before the first frame update
     void Start() {
@@ -16,6 +20,8 @@ public class FarmerAI_Test : MonoBehaviour {
         endPos = waypoints[1].position;
         navigationVector = endPos - currentPos;
         navigationVector = navigationVector.normalized;
+
+        //theta = (Mathf.Atan2(navigationVector.z, navigationVector.x) * Mathf.Rad2Deg - 90)*-1;
     }
 
     // Update is called once per frame
@@ -23,9 +29,12 @@ public class FarmerAI_Test : MonoBehaviour {
 
         currentPos = gameObject.transform.position;
         delta = (currentPos - endPos).magnitude;
+        navigationVector = endPos - currentPos;
+        navigationVector = navigationVector.normalized;
+        //Debug.Log(delta);
 
-        if (delta >= .01)  {
-            transform.Translate(new Vector3(navigationVector.x, transform.position.y, navigationVector.z) * Time.deltaTime * 1);
+        if (delta >= .6)  {
+            transform.Translate(new Vector3(navigationVector.x, transform.position.y, -navigationVector.z) * Time.deltaTime * speed);
         }
         
     }
---
+++ Assets/UIAssets/SpatialSpeechBubble/Assets/Scripts/SpeechBubbleShowPerDistance.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbleShowPerDistance : MonoBehaviour {
+  public Transform target = null;
+  public Transform origin = null;
+
+  private Vector3 vec3InitScale = Vector3.one;
+
+  public float AppearOnDistance = 3.0f;
+  public AnimationCurve xEnterSizeScale;
+  public AnimationCurve yEnterSizeScale;
+  private float xEnterSizeTimePassed = 0.0f;
+  private float yEnterSizeTimePassed = 0.0f;
+
+  public float DisappearOnDistance = 5.0f;
+  public AnimationCurve xExitSizeScale;
+  public AnimationCurve yExitSizeScale;
+  private float xExitSizeTimePassed = float.MaxValue;
+  private float yExitSizeTimePassed = float.MaxValue;
+
+  private bool bInside = false;
+  private bool bOutside = false;
+  private bool bshow = false;
+
+  private float fDistance;
+  private Vector3 vec3DialogScale;
+
+  private void
+  Reset() {
+    origin = transform;
+    target = Camera.main.transform;
+
+    xEnterSizeScale = AnimationCurve.EaseInOut(0.0f, 0.0f, 0.25f, 1.0f);
+    //yEnterSizeScale = AnimationCurve.EaseInOut(0.0f, 0.0f, 0.25f, 1.0f);
+    yEnterSizeScale.AddKey(0.0f, 0.0f);
+    yEnterSizeScale.AddKey(0.2f, 1.5f);
+    yEnterSizeScale.AddKey(0.25f, 1.0f);
+
+    xExitSizeScale = AnimationCurve.Linear(0.0f, 1.0f, 0.1f, 0.0f);
+    yExitSizeScale = AnimationCurve.Linear(0.0f, 1.0f, 0.1f, 0.0f);
+  }
+
+  private void
+  Start() {vec3InitScale = transform.localScale;
+    transform.localScale = Vector3.zero;
+  }
+
+  private void
+  LateUpdate () {
+    fDistance = (target.position - origin.position).magnitude;
+
+    bOutside = fDistance > DisappearOnDistance;
+    bInside = fDistance < AppearOnDistance;
+
+    if (bOutside) {
+      bshow = false;
+    }
+    if (bInside) {
+      bshow = true;
+    }
+
+    vec3DialogScale = vec3InitScale;
+
+    if (bshow) {
+      xExitSizeTimePassed = yExitSizeTimePassed = 0.0f;
+
+      xEnterSizeTimePassed += Time.deltaTime;
+      yEnterSizeTimePassed += Time.deltaTime;
+
+      vec3DialogScale.x = vec3InitScale.x * xEnterSizeScale.Evaluate(xEnterSizeTimePassed);
+      vec3DialogScale.y = vec3InitScale.y * yEnterSizeScale.Evaluate(yEnterSizeTimePassed);
+    }
+    else {
+      xEnterSizeTimePassed = yEnterSizeTimePassed = 0.0f;
+
+      xExitSizeTimePassed += Time.deltaTime;
+      yExitSizeTimePassed += Time.deltaTime;
+
+      vec3DialogScale.x = vec3InitScale.x * xExitSizeScale.Evaluate(xExitSizeTimePassed);
+      vec3DialogScale.y = vec3InitScale.y * yExitSizeScale.Evaluate(yExitSizeTimePassed);
+    }
+
+    transform.localScale = vec3DialogScale;
+	}
+
+  private void
+  OnDrawGizmosSelected() {
+    Color prevCol = Gizmos.color;
+    
+    Gizmos.color = new Color(0.0f, 1.0f, 0.0f, 0.5f);
+    Gizmos.DrawWireSphere(origin.position, AppearOnDistance);
+
+    Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.5f);
+    Gizmos.DrawWireSphere(origin.position, DisappearOnDistance);
+
+    Gizmos.color = prevCol;
+  }
+
+  private void
+  OnValidate() {
+    if (origin == null) {
+      Debug.LogError("origin can't be null!");
+    }
+    if (target == null) {
+      Debug.LogError("target can't be null!");
+    }
+    if (DisappearOnDistance < 0.0f) {
+      DisappearOnDistance = float.MaxValue;
+    }
+    else if (DisappearOnDistance < AppearOnDistance) {
+      Debug.LogError("DisappearOnDistance can't be lower than AppearOnDistance");
+      DisappearOnDistance = AppearOnDistance + 1.0f;
+    }
+  }
+}
