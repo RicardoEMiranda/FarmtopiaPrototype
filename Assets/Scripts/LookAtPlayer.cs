fe2d23288d6d9bd93ad3d3e9a6e96e97dcc23536
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-11-26 12:02:47 -0800

    Speech bubble (code refactor)
    -removed public variable
    -renamed variable
    -assign camera on start
    -Suggest moving camera to FixedUpdate()

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour {
 
-    public Transform camera;
-
-    // Start is called before the first frame update
+    private Transform _camera;
+    
     void Start() {
-        //transform.Rotate(camera.transform.rotation.x, -180f, camera.transform.rotation.z, Space.World);
-        transform.LookAt(camera);
-        //transform.Rotate(camera.transform.rotation.x, -180f, camera.transform.rotation.z, Space.World);
+        _camera = Camera.main.transform;
+        transform.LookAt(_camera);
     }
-
-    // Update is called once per frame
     void LateUpdate() {
-        transform.LookAt(camera);
-        //transform.Rotate(camera.transform.rotation.x, -180f, camera.transform.rotation.z, Space.World);
+        transform.LookAt(_camera);
     }
 }
