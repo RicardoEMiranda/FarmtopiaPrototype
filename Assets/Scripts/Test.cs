fd835ad2f68e8d168b4ae54f06f13da7fc21a533
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-11-30 16:22:25 -0800

    lag

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
     //Todo: Delete this test script
-
-    // Use this for initialization
+    
 
     // Start is called before the first frame update
     void Start()
     {
         //Changes made by LAG 2020-04-08 1:49pm pst
+        //Add this line
     }
     void Update()
     {
-        //more changes bade by LAG 2020-04-08 1:50pm pst
         //Delete this line
     }
     private void FixedUpdate()
