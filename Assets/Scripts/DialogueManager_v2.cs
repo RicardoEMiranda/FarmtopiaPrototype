3ed7e8ed471bc2033773449b7d490c789958300d
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-12-04 00:35:48 -0500

    Refactoring DialogueManager_v2
    - Deleted unneeded code. Starting fresh.
    - Created FarmerAntonio Tag for FarmerAntonio
    - Wrote IEnumerator to handle type texting effect in dialogue bubble

--- Assets/Scripts/DialogueManager_v2.cs
+++ Assets/Scripts/DialogueManager_v2.cs
@@ -6,39 +6,66 @@ using TMPro;
 
 public class DialogueManager_v2 : MonoBehaviour {
 
-    [SerializeField] private TextMeshProUGUI tmpDropText;
-    //public string[] level1Dialogue = new string[1];
-    private string[] level1Dialogue = new string[2];
+    [SerializeField] private TextMeshProUGUI tmpDisplay;
+
+    [TextArea(2,4)]
+    [SerializeField] public string[] level1Dialogue;
     [SerializeField] private Transform level1Waypoints;
     [SerializeField] private GameObject farmHandAntonio;
     [SerializeField] private GameObject canvasSpeechBubble;
+    [SerializeField] public float typingSpeed = .04f;
     private float delta;
+    private int index = 0;
+
+    [SerializeField] public GameObject waypoint;
+    private CollisionDetector collisionDetector;
+
+    [SerializeField] public AudioSource audioSource;
 
     // Start is called before the first frame update
-    void Start() {
-        //tmpDropText.text = "Yo";
-        //gameObject.SetActive(false);
-        //GenerateLevel1Dialogue();
-        level1Dialogue[0] = "Hello, I'm Farmhand Antonio!";
-        level1Dialogue[1] = "Line 2.";
+    void Start() {        
+
+        collisionDetector = waypoint.GetComponent<CollisionDetector>();
     }
 
     // Update is called once per frame
     void Update()  {
-
-        delta = (farmHandAntonio.transform.position - level1Waypoints.transform.position).magnitude;
+        //delta = (farmHandAntonio.transform.position - level1Waypoints.transform.position).magnitude;
         //Debug.Log(delta);
-        if(delta <= .1) {
+        //if(delta <= .1) {
             //Debug.Log("At Waypoint");
-            canvasSpeechBubble.SetActive(true);
-            tmpDropText.text = level1Dialogue[0];
+        //    canvasSpeechBubble.SetActive(true);
+        //}
+
+        if(collisionDetector.hitFarmerAntonio) {
+            ////Debug.Log("Farmer Antonio at Waypoint");
+            collisionDetector.hitFarmerAntonio = false;
+            StartCoroutine(TypeText());
+            audioSource.Play();
         }
     }
 
-    private void GenerateLevel1Dialogue()  {
-        //Debug.Log("Generate Dialogue");
-        //level1Dialogue[0] = "Hello, I'm Farmhand Antonio!";
-        //level1Dialogue[1] = "You can click on the field <br> next to me to select what <br> hemp seed varietal you want to <br>" +
-        //    "plant. Go ahead, try it!";
+    IEnumerator TypeText() {
+        foreach (char letter in level1Dialogue[index].ToCharArray()) {
+            tmpDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        audioSource.Stop();
+    }
+
+    public void OnNextButtonDown() {
+        //Debug.Log("Next");
+
+        if(index < level1Dialogue.Length -1)  {
+            index++;
+            tmpDisplay.text = ""; //reset the text before printing more
+            StartCoroutine(TypeText());
+            audioSource.Play();
+        }
+        else  {
+            tmpDisplay.text = "";
+            canvasSpeechBubble.SetActive(false);
+        }
+        
     }
 }
