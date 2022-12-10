0f94d2b6e1fa4d29d5d19f44703cbcf4395e57ba
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-12-10 18:49:26 -0500

    Cleaned up TractorAI_.cs used for the tractor in the LandNFT scene
    - Took out comments that aren't needed

--- Assets/Scripts/TractorAI_.cs
+++ Assets/Scripts/TractorAI_.cs
@@ -19,27 +19,19 @@ public class TractorAI_ : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //Debug.Log(orientation);
-        //transform.Translate(new Vector3(0,0,1)*Time.deltaTime*speed*direction);
+       
         currentPos = transform.position;
         transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed * direction);
 
-        if (gameObject.transform.position.z >= startPos.z && gameObject.transform.position.z <= 12.5)  {
-            //transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            //transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed * direction);
-     
-        }
-
-        if(gameObject.transform.position.z >= 12.5)  {
-            //orientation = 0;
+        
+        if(gameObject.transform.position.z >= 12.5) { 
             transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-            //transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed * direction);
         }
 
         if (gameObject.transform.position.z <= -5.465)  {
-            //orientation = 0;
+            
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            //transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed * direction);
+           
         }
 
 
