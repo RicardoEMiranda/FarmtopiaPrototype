8203a73981b5baa25a9ef8d4c503da6e2d697850
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-12-12 17:01:29 -0500

    Signup Modal
    Created a signup modal that will overlay on the Login page when player selects the "Signup" link.
    
    Deleted the Signup scene since it's no longer needed.

---
+++ Assets/Scripts/UserManagement/LoginManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginManager  {
+
+    public string Email;
+    private string userName;
+
+    private void GetUserName()  {
+
+    }
+
+    private void OnLogin() {
+
+        //Check that string entered is a proper email string
+
+    }
+
+    
+}
