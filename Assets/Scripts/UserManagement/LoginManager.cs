5c29acf5e6fbdec10ca551e917bee139f18ddaa8
Ricardo Miranda <ricardo.e.miranda@icloud.com>
2022-12-12 22:25:29 -0500

    Login Manager and Scene updates

--- Assets/Scripts/UserManagement/LoginManager.cs
+++ Assets/Scripts/UserManagement/LoginManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class LoginManager  {
+public class LoginManager : MonoBehaviour  {
 
-    public string Email;
+    [SerializeField] public GameObject signupPanel;
+    [SerializeField] public GameObject errorPanel;
+    [SerializeField] public TextMeshProUGUI emailInput;
+    public string email;
     private string userName;
 
     private void GetUserName()  {
@@ -15,6 +20,37 @@ public class LoginManager  {
 
         //Check that string entered is a proper email string
 
+    }
+
+    public void OnSignUpLink () {
+
+        //Activate the SignupPanel
+        signupPanel.SetActive(true);
+    }
+
+    public void OnSignUpButton() {
+
+        email = emailInput.text;
+
+        if (email.Contains('@'))  {
+            Debug.Log("Yes");
+            errorPanel.SetActive(false);
+            //Check that the email entered is properly formatted
+            string[] emailParts = email.Split('@');
+            string beforeAt = emailParts[0];
+            string afterAt = emailParts[1];
+        }
+        if(!email.Contains('@'))  {
+            errorPanel.SetActive(true);
+            //emailInput.text = "";
+            Debug.Log("Improper email format");
+        }
+
+        
+    }
+
+    public void OnGoBackToLogin()  {
+        signupPanel.SetActive(false);
     }
 
     
